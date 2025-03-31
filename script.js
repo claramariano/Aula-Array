e85b63edc4d18e1b7245217108b981e0fc7ff799
claramariano <claraamariano@gmail.com>
2025-03-31 08:48:22 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+//criar o vetor ARRAY
+
+var nomes = ["Clara", "Malu", "Laura", "Anna", "Julia"]
+var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 4 "))
+
+//concatenar
+document.write ("Nome escolhido: " + nomes[nomeEscolhido])
+   
